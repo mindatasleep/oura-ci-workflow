4680e5cd10740cb7db77d88095aca15c4eefe4d5
mindatasleep <mindatasleep@gmail.com>
2020-01-30 10:50:12 -0700

    Deploy stage, not build.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ ssh -vvv -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
     export $(cat .env | xargs)
     docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
     docker pull $IMAGE:web
-    docker-compose -f ./docker-compose.ci.yml up -d
+    docker-compose -f docker-compose.ci.yml up -d
 ENDSSH
