97c0d8b94fb2e5019355fdd3ccfd2fba2f013d99
mindatasleep <mindatasleep@gmail.com>
2020-01-30 11:12:10 -0700

    Deploy stage, not build.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ssh -vvv -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+    cd /app
     export $(cat .env | xargs)
     docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
     docker pull $IMAGE:web
--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -4,5 +4,18 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /webapp/tmp/pids/server.pid
 
+# Check for database
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
+
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
+
+#     echo "PostgreSQL started"
+# fi
+
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
