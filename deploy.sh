774db583560fc28bdadfc24a8d6e355aca753d42
mindatasleep <mindatasleep@gmail.com>
2020-01-30 13:10:34 -0700

    Create and route Rails homepage.

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ ssh -vvv -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
     docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
     docker pull $IMAGE:web
     docker-compose -f docker-compose.ci.yml up -d
+    docker container exec default_web_1 rails db:create db:migrate
 ENDSSH
