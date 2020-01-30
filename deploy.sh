fd357da76405ad0ae41a2cb6bc96b34270b62076
mindatasleep <mindatasleep@gmail.com>
2020-01-30 11:30:43 -0700

    Deploy stage, not build.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh -vvv -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
-    cd /app
+    cd ..
     export $(cat .env | xargs)
     docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
     docker pull $IMAGE:web
